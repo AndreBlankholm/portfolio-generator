b49cc89bb325760bc0b3b0e5591a272728e58763
AndreBlankholm <ablankholm@gmail.com>
2022-04-28 16:38:18 -0500

    about to create an html page

--- app.js
+++ app.js
@@ -1,21 +1,15 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+const profileDataArgs = process.argv.slice(2);
 
-const printProfileData = profileDataArr => {  //printing command line arument one at a time with this function
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
-    }
-    console.log('================');
-
-    profileDataArr.forEach((profileItem) => {
-      console.log(profileItem)
-    });
-
-  };
-
-  
-printProfileData(profileDataArgs);
+const [name, github] = profileDataArgs;
 
 
 
 
+//console.log(generatePage(name, github));
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
